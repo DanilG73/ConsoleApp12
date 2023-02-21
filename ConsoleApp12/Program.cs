7cfcd83b9205237219c0ae28c637748cc6dd1c62
DanilG73 <123488618+DanilG73@users.noreply.github.com>
2023-02-21 20:51:59 +0200

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.
+            //int[] myarray1 = new int[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"myarray1[{i}]=");
+            //    myarray1[1] = int.Parse(Console.ReadLine());
+            //}
+            //int max = myarray1[0];
+            //int index = 0;
+            //for (int i = 0; i < myarray1.Length; i++)
+            //{
+            //    if (myarray1[i] > max) ;
+
+            //}
+            //Console.WriteLine($"max={max}\tindex={index}");
+            Console.ReadKey();
+        }
+    }
+}
